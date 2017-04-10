fb24ffdf46889ca66543fa30525f936a16ea9c47
mikelubs <michaelubembe@gmail.com>
2017-04-10 17:16:57 +0300

    create new directories for css javascript and images

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
 
-  $("div#click-one").click(function(event) {
-    var whatToSay = "Hello!";
-    alert(whatToSay);
-  });
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
 
-  $("div#click-two").click(function(event) {
-    alert(whatToSay);
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
